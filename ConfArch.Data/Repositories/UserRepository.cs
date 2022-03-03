086014c72d08ba7f1d6ec97fd9d71cbf8928aed7
princejk <kehindejohn018@gmail.com>
2022-03-03 08:22:15 +0100

    Implemented userRepository

---
+++ ConfArch.Data/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using ConfArch.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfArch.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private List<User> users = new List<User>
+        {
+            new User { Id = 3522, Name = "roland", Password = "K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=",
+                FavoriteColor = "blue", Role = "Admin", GoogleId = "101517359495305583936" }
+        };
+        public User GetByUsernameAndPassword(string username, string password)
+        {
+            var user = users.SingleOrDefault(u => u.Name == username &&
+                                                  u.Password == password.Sha256());
+            return user;
+        }
+
+        public User GetByGoogleId(string googleId)
+        {
+            var user = users.SingleOrDefault(u => u.GoogleId == googleId);
+            return user;
+        }
+    }
+}
