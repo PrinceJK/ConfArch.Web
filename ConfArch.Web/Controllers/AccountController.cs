b7f1853de6d9b914a368b75b71d89e669567eb87
princejk <kehindejohn018@gmail.com>
2022-03-03 08:13:36 +0100

    added user model

---
+++ ConfArch.Web/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfArch.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserRepository userRepository;
+    }
+}
